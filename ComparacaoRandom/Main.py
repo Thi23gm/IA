65b2f557a40c03a4082a8e7483374ab3ff369208
Thiago Gomes <thi23gm@gmail.com>
2023-09-15 11:17:52 -0300

    Random Forest comparacoes

---
+++ ComparacaoRandom/Main.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import io
+from sklearn.model_selection import (
+    train_test_split,
+    GridSearchCV,
+    RandomizedSearchCV,
+    StratifiedKFold,
+)
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import CategoricalNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
+
+# Dados
+data = """
+dia,aparencia,temperatura,umidade,ventando,jogar
+d1,sol,Quente,Alta,nao,nao
+d2,sol,Quente,Alta,sim,nao
+d3,Nublado,Quente,Alta,nao,sim
+d4,Chuva,Agradavel,Alta,nao,sim
+d5,Chuva,Fria,Normal,nao,sim
+d6,Chuva,Fria,Normal,sim,nao
+d7,Nublado,Fria,Normal,sim,sim
+d8,sol,Agradavel,Alta,nao,nao
+d9,sol,Fria,Normal,nao,sim
+d10,Chuva,Agradavel,Normal,nao,sim
+d11,sol,Agradavel,Normal,sim,sim
+d12,Nublado,Agradavel,Alta,sim,sim
+d13,Nublado,Quente,Normal,nao,sim
+d14,Chuva,Agradavel,Alta,sim,nao
+"""
+
+df = pd.read_csv(io.StringIO(data), sep=",")
+
+label_encoders = {}
+for column in df.select_dtypes(include=["object"]).columns:
+    le = LabelEncoder()
+    df[column] = le.fit_transform(df[column])
+    label_encoders[column] = le
+
+X = df.drop(["dia", "jogar"], axis=1)
+y = df["jogar"]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
+
+# Modelos
+rf = RandomForestClassifier()
+nb = CategoricalNB()
+dt = DecisionTreeClassifier()
+
+# StratifiedKFold
+stratified_kfold = StratifiedKFold(n_splits=3, shuffle=True, random_state=42)
+
+# Hiperparâmetros
+param_grid_rf = {
+    "n_estimators": [10, 50, 100, 200],
+    "max_features": ["sqrt", "log2"],
+    "max_depth": [2, 4, 6, 8, 10, None],
+    "bootstrap": [True, False],
+}
+param_grid_dt = {
+    "criterion": ["gini", "entropy"],
+    "splitter": ["best", "random"],
+    "max_depth": [2, 4, 6, 8, 10, None],
+}
+
+# Buscas
+rf_grid_search = GridSearchCV(rf, param_grid_rf, cv=stratified_kfold)
+rf_random_search = RandomizedSearchCV(rf, param_grid_rf, n_iter=10, cv=stratified_kfold)
+
+dt_grid_search = GridSearchCV(dt, param_grid_dt, cv=stratified_kfold)
+dt_random_search = RandomizedSearchCV(dt, param_grid_dt, n_iter=10, cv=stratified_kfold)
+
+# Fit
+rf_grid_search.fit(X_train, y_train)
+rf_random_search.fit(X_train, y_train)
+
+dt_grid_search.fit(X_train, y_train)
+dt_random_search.fit(X_train, y_train)
+
+nb.fit(X_train, y_train)
+
+# Predições e acurácias
+models = {
+    "Random Forest GridSearch": rf_grid_search,
+    "Random Forest RandomSearch": rf_random_search,
+    "Decision Tree GridSearch": dt_grid_search,
+    "Decision Tree RandomSearch": dt_random_search,
+    "Naive Bayes": nb,
+}
+
+for name, model in models.items():
+    if "Naive" in name:
+        y_pred = model.predict(X_test)
+    else:
+        y_pred = model.best_estimator_.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"Acerto do {name} : {accuracy:.4f}")
